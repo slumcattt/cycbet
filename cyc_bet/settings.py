4347625f8923e94e7b787ed53265fa9f08c746da
Aiden <aiden3189@gmail.com>
2014-06-05 15:56:45 -0500

    changed settings for diff os's and added Pillow and models

--- cyc_bet/settings.py
+++ cyc_bet/settings.py
@@ -78,7 +78,7 @@ else:
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES['default'] =  dj_database_url.config()
-    DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+    #DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
